37e11ba3855185d38424f399afe236f4a043bd2c
Faizan <Faizan@LAPTOP-HGJ4MOUP>
2026-01-21 09:39:34 +0530

    Add project files.

---
+++ Github/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Github
+{
+    internal class Program
+    {
+        public void Dispay()
+        {
+            Console.WriteLine("Welcome to first git project");
+        }
+        static void Main(string[] args)
+        {
+            Program p=new Program();
+            p.Dispay();
+            Console.ReadKey();
+        }
+    }
+}
